05b9b66fe9fb0222400c4ad09da18a5b20140e4d
yuyeon329 <peachberryjr@gmail.com>
2021-05-30 12:45:02 +0900

    Programmers 행렬의 덧셈 add

---
+++ level1/행렬의 덧셈/solution.py
@@ -0,0 +1,9 @@
+def solution(arr1, arr2):
+    answer = []
+    for i in range(len(arr1)):
+        sum = []
+        for j in range(len(arr1[1])):
+            sum.append(arr1[i][j] + arr2[i][j])
+        answer.append(sum)
+    #answer = [[c + d for c,d in zip(a,b)] for a,b in zip(arr1, arr2)]
+    return answer
