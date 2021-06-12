381d79fd6d3033fc429109fb367ddc007678337a
yuyeon329 <peachberryjr@gmail.com>
2021-06-12 11:50:56 +0900

    Programmers 체육복 add

---
+++ level1/체육복/solution.py
@@ -0,0 +1,9 @@
+def solution(n, lost, reserve):
+    new_reserve = set(reserve) - set(lost)
+    new_lost = set(lost) - set(reserve)
+    for i in new_reserve:
+        if i-1 in new_lost:
+            new_lost.remove(i-1)
+        elif i+1 in new_lost:
+                new_lost.remove(i+1)
+    return n - len(new_lost)
