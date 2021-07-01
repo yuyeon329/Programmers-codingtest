6fb22b01a528702b6b33896c31a3e25c35ae49b9
yuyeon329 <peachberryjr@gmail.com>
2021-07-01 18:00:17 +0900

    Programmers 프렌즈4블록 update

--- level2/프렌즈4블록/solution.py
+++ level2/프렌즈4블록/solution.py
@@ -1,27 +1,43 @@
 def solution(m, n, board):
-    board = list(map(list, zip(*board)))
-    
-    def game(b):
-        pops = 0
-        sc = [i[:] for i in b]
-        for i in range(n-1):
-            for j in range(m-1):
-                if b[i][j] == -1: continue
-                if b[i][j] == b[i+1][j] == b[i+1][j+1] == b[i][j+1]:
-                    sc[i][j], sc[i+1][j], sc[i+1][j+1], sc[i][j+1] = 0,0,0,0
-        
-        for i,v in enumerate(sc):
-            cnt = v.count(0)
-            pops += cnt
-            sc[i] =[-1]*cnt + [a for a in v if a!=0]
-        
-        return sc, pops
-    
     answer = 0
-    
-    while True:
-        board, pops = game(board)
+    #board를 리스트로 바꿔주기
+    for i in range(len(board)):
+        board[i] = list(board[i])
+    while True: #블록을 지울 수 있을 때까지 계속 실행된다.
+        #board와 같은 크기의 remove선언 후, 모양이 같은 2x2블럭의 위치를  remove에 1로 표시한다.
+        remove = [[0]*n for _ in range(m)]
+        for i in range(m-1):
+            for j in range(n-1): #2x2블록의 중첩 가능
+                #블록이 존재하고 2x2블록의 모양이 같으면
+                if board[i][j] != 0 and board[i][j] == board[i][j+1] == board[i+1][j] == board[i+1][j+1]:
+                    remove[i][j], remove[i][j+1], remove[i+1][j], remove[i+1][j+1] = 1,1,1,1
+        count = 0
         
-        if pops == 0:  return answer
+        #remove를 순회하며 지워진 블록 갯수 세기
+        for i in range(m):
+            count += sum(remove[i])
+        answer += count    
         
-        answer += pops
+        #만약 지워진 블록이 없다면 break 
+        if count == 0:
+            break
+        
+        #지워진 블록으로 인해 빈 공간 채우기
+        """
+        좌하단부터 탐색하며 2x2가 1인 경우에 블록을 땡겨준다. 
+        여기서 처음 지워줄 경우엔 위에 있는 블록이 아래로 떨어지는 것이고, 
+        블록이 떨어지고 난 후 빈 블록칸을 1과 0을 이용해 표시한다.
+        아래의 For문이 블록이 떨어지는 상황 자체를 표현한 것이다.
+        """
+        for i in range(m-1, -1, -1):
+            for j in range(n): #좌하단부터 탐색
+                if  remove[i][j] == 1: #만약 1이면
+                    x = i - 1 
+                    while x >= 0 and remove[x][j] == 1: 
+                        x -=1 
+                    if x < 0 :
+                        board[i][j] = 0 
+                    else :
+                        board[i][j] = board[x][j] 
+                        remove[x][j] = 1    
+    return answer
